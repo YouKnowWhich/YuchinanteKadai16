9719caa537f69e30dd07be4f879c761a5048e803
yuya-3110 <ysuu45143745@gmail.com>
2024-05-11 15:32:12 +0900

    追加モードと編集モードを1画面で実装。ただし、Save、Cancelを押下しても画面遷移しない。

--- YuchinanteKadai16/AddItemViewController.swift
+++ YuchinanteKadai16/AddItemViewController.swift
@@ -9,13 +9,41 @@
 import UIKit
 
 class AddItemViewController: UIViewController {
+
+    // 追加モードと編集モードを定義する列挙体
+    enum Mode {
+        case Add, Edit
+    }
+
+    var mode = Mode.Add  // デフォルトは追加モード
+
+
     // アイテム名を入力するテキストフィールド
     @IBOutlet private weak var nameTextField: UITextField!
 
-    // テキストフィールドに入力された名前を取得するプロパティ
-    var name: String {
+    var name = ""  // 入力されたアイテム名を保持するプロパティ
 
-        // テキストフィールドのテキストがnilの場合は空文字を返す
-        nameTextField.text ?? ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 編集モードの場合はテキストフィールドに既存のアイテム名を表示する
+        if mode == .Edit {
+            nameTextField.text = name
+        }
+    }
+
+    // セーブボタンが押された時の処理
+    @IBAction func pressSaveButton(_ sender: Any) {
+        let identifier = (mode == .Add) ? "exitFromAddBySaveSegue" : "exitFromEditBySaveSegue"
+        // セグエを実行して遷移する
+        performSegue(withIdentifier: identifier, sender: sender)
+    }
+
+    // キャンセルボタンが押された時の処理
+    @IBAction func pressCancelButton(_ sender: Any) {
+        let identifier = (mode == .Add) ? "exitFromAddByCancelSegue" : "exitFromEditByCancelSegue"
+        // セグエを実行して遷移する
+        performSegue(withIdentifier: identifier, sender: sender)
     }
 }
--- YuchinanteKadai16/TableViewController.swift
+++ YuchinanteKadai16/TableViewController.swift
@@ -14,6 +14,9 @@ class TableViewController: UITableViewController {
     private let keyName = "Name"
     private let keyCheck = "Check"
 
+    // アクセサリボタンがタップされたセルのインデックスパスを保持するプロパティ
+    var editIndexPath: IndexPath?
+
     // テーブルビューに表示するアイテムの配列
     private var items: [[String: Any]] = []
 
@@ -32,51 +35,90 @@ class TableViewController: UITableViewController {
 
     // セルの数を返すメソッド
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        items.count
+        return items.count
     }
 
     // セルを構築するメソッド
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
         // セルを取得し、カスタムセルにダウンキャスト
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath) as! ItemTableViewCell
-
         // アイテムを取得
         let item = items[indexPath.row]
-
         // セルの表示を設定するメソッドを呼び出してセルを更新
         cell.configure(name: (item[keyName] as? String) ?? "", isChecked: (item[keyCheck] as? Bool) ?? false)
-
         // 更新されたセルを返す
         return cell
     }
 
     // ユーザーがテーブルビューのセルを選択した時の処理
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
         // チェック状態を反転させる
         if let check = items[indexPath.row][keyCheck] as? Bool {
             items[indexPath.row][keyCheck] = !check
-
             // テーブルビューのセルを再読み込みして更新する
             tableView.reloadRows(at: [indexPath], with: .automatic)
         }
     }
 
-    // Cancelボタンがタップされた時に呼ばれるメソッド
-    @IBAction func didTapCancelButton(segue: UIStoryboardSegue) {
+    // アクセサリボタンがタップされた時の処理
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        print("accessoryButtonTappedForRowWith")
+        // 編集対象のセルのインデックスパスを保持する
+        editIndexPath = indexPath
+        // 編集画面に遷移する
+        performSegue(withIdentifier: "EditSegue", sender: indexPath)
     }
 
-    // AddItemViewControllerからの遷移によるデータの追加処理を行うメソッド
-    @IBAction func didTapAddByButton(segue: UIStoryboardSegue) {
+    // セグエが実行される直前の処理
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let add = (segue.destination as? UINavigationController)?.topViewController as? AddItemViewController {
+            switch segue.identifier ?? "" {
+            case "AddSegue":
+                // 追加モードを設定する
+                add.mode = AddItemViewController.Mode.Add
+                break
+            case "EditSegue":
+                // 編集モードを設定する
+                add.mode = AddItemViewController.Mode.Edit
+                if let indexPath = sender as? IndexPath {
+                    let item = self.items[indexPath.row]
+                    // 編集対象のアイテム名を渡す
+                    add.name = (item[keyName] as? String) ?? ""
+                }
+                break
+            default:
+                break;
+            }
+        }
+    }
+
+    // 追加モードを押された時のボタン
+    @IBAction func exitFromAddByCancel(segue: UIStoryboardSegue) {
+    }
+    @IBAction func exitFromAddBySave(segue: UIStoryboardSegue) {
         if let add = segue.source as? AddItemViewController {
 
             // アイテム名とチェック状態を辞書型に格納し、itemsに追加する
             let item: [String: Any] = [keyName: add.name, keyCheck: false]
             items.append(item)
+            let indexPath = IndexPath(row: items.count - 1, section: 0)
+            // テーブルビューに行を追加する
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
 
-            // テーブルビューをリロードして表示を更新する
-            tableView.reloadData()
+    // 編集モードを押された時のボタン
+    @IBAction func exitFromEditByCancel(segue: UIStoryboardSegue) {
+    }
+
+    @IBAction func exitFromEditBySave(segue: UIStoryboardSegue) {
+        if let add = segue.source as? AddItemViewController {
+            if let indexPath = editIndexPath {
+                // アイテム名を更新する
+                items[indexPath.row][keyName] = add.name
+                // テーブルビューの行を再読み込みする
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
         }
     }
 }
